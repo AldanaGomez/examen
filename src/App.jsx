fba92a258c1a06cb7c21088bc94ff0c6064172fa
AldanaGomez <afg@bb.vision>
2022-11-09 21:26:45 -0300

    evaluacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Estatus from './components/Estatus';
+import Posteos from './components/Posteos';
 // El componente App es el padre de:
 // - Estatus
 // - Posteos
@@ -8,10 +10,16 @@ import React from 'react'
 
 function App() {
 
+  const[totalLikes, setTotalLikes] = useState(0)
+
+  const aumentar = ()=>{
+    setTotalLikes(totalLikes + 1)
+  }
+
   return (
     <div className="App">
-      <Estatus />
-      <Posteos />
+      <Estatus totalLikes = {totalLikes}/>
+      <Posteos aumentar = {aumentar} />
     </div>
   );
 }
